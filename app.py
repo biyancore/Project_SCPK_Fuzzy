153878e73abcf7bda979b270a0c112193e6ed083
Briliant Priscilla <briliantpriscilla30@gmail.com>
2026-05-14 01:39:13 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+# (Di sini nanti Andini tinggal tambahkan layout Streamlit-nya)
+st.title("Pemilihan Moda Transportasi Darurat untuk Distribusi Obat ke Daerah Terpencil Saat Bencana")
+st.write("Dibuat oleh: Biyan & Andini")
